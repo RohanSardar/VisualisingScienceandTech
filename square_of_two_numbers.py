83b142373e216bea112d35a7f086fed6a9b757c0
RohanSardar <77870108+RohanSardar@users.noreply.github.com>
2021-01-28 21:13:32 +0530

    Add files via upload

---
+++ square_of_two_numbers.py
@@ -0,0 +1,10 @@
+#Program to square a number
+
+import math
+
+x = int(input("Enter the number which is to be squared: "))
+
+square = math.pow (x, 2)
+#Here x is the variable and 2 is the power which is the square
+
+print ("The square of the number", x, "is", square)
