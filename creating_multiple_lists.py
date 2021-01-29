5ee7a55fe963b7e9e5a888ecd226d4d35bb58080
RohanSardar <77870108+RohanSardar@users.noreply.github.com>
2021-01-29 16:37:46 +0530

    Add files via upload

---
+++ creating_multiple_lists.py
@@ -0,0 +1,12 @@
+# Program to create one/multiple lists in Python
+
+collection1 = ["Apple", "Coconut", "Mango", "Papaya"]
+collection2 = [1, 5, 9, 7]
+collection3 = ["Windows 10", "Windows 8.1"]
+#Here you can make list by choosing any suitable variable
+#For strings you have to use "", but that is not required for no.s
+
+print (collection1)
+print (collection2)
+print (collection3)
+#From here you can print your created lists
