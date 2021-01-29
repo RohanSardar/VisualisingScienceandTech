3750027b9c781e9c10a748e8b208c8bc0d36eb5f
RohanSardar <77870108+RohanSardar@users.noreply.github.com>
2021-01-29 20:14:50 +0530

    Add files via upload

---
+++ square_root_of_one_no.py
@@ -0,0 +1,11 @@
+# Program to find the square root of a user given number
+
+import math
+#To square root a number it is essential to import math
+
+x = int(input("Enter a number: "))
+#Here x is the variable which contains the input from user
+
+print ("The square of the number", x, "is", math.sqrt(x))
+#math.sqrt is the funcrion name and (x) is the number
+#which is to be square rooted
