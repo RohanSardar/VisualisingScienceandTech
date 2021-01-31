a3fec8ea31c73064c13fa968118f00eeca966e1d
RohanSardar <77870108+RohanSardar@users.noreply.github.com>
2021-01-31 09:15:59 +0530

    Add files via upload

---
+++ changing_list_item.py
@@ -0,0 +1,13 @@
+# Program to change the items of a created list in Python
+
+#First create a list and print its contents
+mylist = ["HP", "Dell", "Lenovo", "Apple", "Asus"]
+print (mylist)
+
+#Now lets change "Apple" to "Acer"
+mylist [3] = "Acer"
+#Note, here 0 means 1, 1 means 2 and so on. That's why 3 is the 
+#4th value of the list
+
+print (mylist)
+#This will print the new list with changed item
