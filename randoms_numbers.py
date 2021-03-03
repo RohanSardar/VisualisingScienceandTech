c2588b7f9e6e9edefa407a88930c78aeeaa93cac
RohanSardar <77870108+RohanSardar@users.noreply.github.com>
2021-03-03 18:13:57 +0530

    Add files via upload

---
+++ randoms_numbers.py
@@ -0,0 +1,9 @@
+#Program to print any random integer by specifying the value
+
+import random
+#You have to import random library to do this feature
+
+x = random.randint(1,10)
+#Here x is the variable and the range is 1 to 10
+
+print (x)
