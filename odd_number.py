7adab6114213e703a96ade47850753b7e152ac12
RohanSardar <77870108+RohanSardar@users.noreply.github.com>
2021-03-05 20:25:39 +0530

    Add files via upload

---
+++ odd_number.py
@@ -0,0 +1,10 @@
+# Program to find the odd numbers in a certain range of number
+
+for number in range (1,50):
+    #Here I have taken a range of numbers from 1 to 50
+    if (number%2!=0):
+        #Here "%2" is used to find the remainders of all the numbers in 
+        #range after dividing by 2 & "!0" is used to represent not equals to
+        print (number)
+        
+        
