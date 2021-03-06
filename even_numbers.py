401e8b55c7fe47c96c75b1986659d02a19829e65
RohanSardar <77870108+RohanSardar@users.noreply.github.com>
2021-03-06 11:48:35 +0530

    Add files via upload

---
+++ even_numbers.py
@@ -0,0 +1,8 @@
+#Program to find even numbers in a certain range of numbers
+
+for number in range (1,50):
+    #Here the range is from 1 to 50
+    if (number%2!=1):
+        #here %2 means the remainder of the numbers after dividing by 2
+        #and != means not equals to
+        print (number)
